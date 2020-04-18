d6cfd7ecd1d42c8276104ae0e03cbd324bd2e050
ivnard <ivan.nardini@sas.com>
2020-04-18 16:15:15 +0200

    Documenting setup.sh

--- 0_setup/setup.sh
+++ 0_setup/setup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-#Pass REGION and BUCKET name
+# setup.sh
+# Create a buckek if doesn't exist. And load deployment scripts (.sh, .py)
+# REGION  - Region name (default eu)
+# BUCKET - Bucker name (default cloud-demo-databrick-gcp)
 
-#Create a BUCKET if not exist
+
+#Pass REGION and BUCKET names (or use default parameters)
+REGION=${1:-eu}
+BUCKET=${2:-cloud-demo-databrick-gcp}
+
+#Create a BUCKET name if not exist
 if ! gsutil ls | grep -q gs://${BUCKET}/; then
   gsutil mb -l ${REGION} gs://${BUCKET}
-  #upload cluster_init.sh, boston_house_prices_toscore.csv and Boston_lrModel_mleap.zip, score.py
+  #Upload cluster_init.sh, boston_house_prices_toscore.csv and Boston_lrModel_mleap.zip, score.py
   gsutil cp /home/ivan_nardini/Databricks_MLflow_GCP/0_setup/setup_cluster.sh gs://${BUCKET}/0_init/init.sh
   gsutil cp /home/ivan_nardini/Databricks_MLflow_GCP/1_data/boston_house_prices.csv gs://${BUCKET}/1_data/boston_house_prices_toscore.csv
   gsutil cp /home/ivan_nardini/Databricks_MLflow_GCP/2_notebooks/output/ModelProjects_Boston_ML_lrModel.zip gs://${BUCKET}/2_model/Boston_lrModel_mleap.zip
