062153521be9042f04d6aca3c271c20d98dfb1bb
ivnard <ivan.nardini@sas.com>
2020-04-18 17:11:34 +0200

    Typo setup_cluster.sh

--- 0_setup/setup_cluster.sh
+++ 0_setup/setup_cluster.sh
@@ -16,15 +16,7 @@ ZONE=${4:-europe-west6-a}
 gcloud beta dataproc clusters create ${CLUSTER_NAME} \
 --bucket ${BUCKET} \
 --region ${REGION} \
---subnet default \
---no-address \
 --zone ${ZONE} \
---master-machine-type n1-standard-4 \
---master-boot-disk-size 500 \
---num-workers 2 \
---worker-machine-type n1-standard-4 \
---worker-boot-disk-size 500 \
---image-version 1.3-deb9 \
 --optional-components ANACONDA,JUPYTER \
 --scopes 'https://www.googleapis.com/auth/cloud-platform' \
 --project gel-sassandbox \
