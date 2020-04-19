089139911f36a5e2f1472707fbc4dbd01e1fddae
ivnard <ivan.nardini@sas.com>
2020-04-19 13:29:56 +0200

    Test done. I can score in Google Dataproc

--- 0_setup/cluster_config/init.sh
+++ 0_setup/cluster_config/init.sh
@@ -4,13 +4,13 @@
 # Install spark dependencies. Install mleap pyspark. Download Model.zip
 
 # Install Dependencies
-spark-shell --packages ml.combust.mleap:mleap-spark_2.11:0.15.0
+#spark-shell --packages ml.combust.mleap:mleap-spark_2.11:0.15.0
 
 # Install python libraries
-pip install mleap==0.15.0 pyspark==2.4.5
+#pip install mleap==0.15.0 pyspark==2.4.5
 
 # Move in the right jars folder (to check)
-mv /root/.ivy2/jars/*.jar /usr/lib/spark/jars
+#mv /root/.ivy2/jars/*.jar /usr/lib/spark/jars
 
 # Download Model
 curl -X GET https://github.com/IvanNardini/Databricks_MLflow_GCP/raw/master/2_notebooks/output/ModelProjects_Boston_ML_lrModel.zip --output /tmp/model.zip
