5ad6b57d0ac22cfea90d2d149851870b6f42a1ed
ivnard <ivan.nardini@sas.com>
2020-04-20 09:06:20 +0200

    Updates all shell scripts

--- 0_setup/1_setup_bucket.sh
+++ 0_setup/1_setup_bucket.sh
@@ -12,13 +12,16 @@ BUCKET=${2:-cloud-demo-databrick-gcp}
 
 #Create a BUCKET name if not exist
 if ! gsutil ls | grep -q gs://${BUCKET}/; then
-  gsutil mb -l ${REGION} -b on gs://${BUCKET}
-  #Upload cluster_init.sh, boston_house_prices_toscore.csv and Boston_lrModel_mleap.zip, score.py
-  gsutil cp /home/ivan_nardini/Databricks_MLflow_GCP/0_setup/setup_cluster.sh gs://${BUCKET}/0_init/init.sh
-  gsutil cp /home/ivan_nardini/Databricks_MLflow_GCP/1_data/boston_house_prices.csv gs://${BUCKET}/1_data/boston_house_prices_toscore.csv
+  gsutil mb -l ${REGION} -b on gs://${BUCKET} 
+  #Upload init.sh, boston_house_prices_toscore.csv and Boston_lrModel_mleap.zip, score.py
+
+  gsutil cp /home/ivan_nardini/Databricks_MLflow_GCP/0_setup/cluster_config/init.sh gs://${BUCKET}/0_setup/cluster_config/init.sh
+  gsutil cp /home/ivan_nardini/Databricks_MLflow_GCP/1_data/boston_house_prices.csv gs://${BUCKET}/1_data/boston_house_prices_toscore.csv 
   gsutil cp /home/ivan_nardini/Databricks_MLflow_GCP/2_notebooks/output/ModelProjects_Boston_ML_lrModel.zip gs://${BUCKET}/2_model/Boston_lrModel_mleap.zip
   gsutil cp /home/ivan_nardini/Databricks_MLflow_GCP/2_notebooks/output/score.py gs://${BUCKET}/2_model/score.py
 fi
 
 #Check BUCKET content
 gsutil ls -r gs://${BUCKET}/**
+
+
